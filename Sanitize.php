f56d794c547b6a4e7e4b9dfae939e22e4dbce153
Francisco <andrewplanet@gmail.com>
2017-09-07 17:53:38 -0400

    Added User model and lib Sanitize to escape html chars

---
+++ Sanitize.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+*  Sanitize
+*/
+class Sanitize
+{
+
+	public static function html($string) {
+		return htmlspecialchars($string);
+	}
+
+}
---
+++ User.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'Sanitize.php';
+
+/**
+* User Model
+*/
+class User
+{
+	
+	private $name;
+	private $email;
+	private $hash;
+
+	public function setName(string $name) {
+		$this->name = Sanitize::html($name);
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setEmail(string $email) {
+		$this->email = Sanitize::html($email);
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function setPassword(string $password) {
+		$this->hash = password_hash($password, PASSWORD_DEFAULT);
+	}
+
+	public function dump() {
+		var_dump($this->name, $this->email, $this->hash);
+	}
+
+}
+
+$user = new User();
+$user->setName("Frank");
+$user->setEmail("frandresgo@gmail.com");
+$user->setPassword("secretpassword123456789");
+$user->dump();
